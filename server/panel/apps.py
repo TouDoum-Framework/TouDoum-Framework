b4409ab0ee52c386d45be6bab5f91cd84f4774ec
msterhuj <gabin.lanore@gmail.com>
2021-05-31 20:21:51 +0200

    Work on nav

--- server/panel/apps.py
+++ server/panel/apps.py
@@ -1,5 +1,11 @@
+import hashlib
+
 from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     name = 'panel'
+
+
+def get_avatar(email: str):
+    return "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower().encode("utf8")) + "?d=retro&f=y"
