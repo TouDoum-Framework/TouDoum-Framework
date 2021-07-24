08da54ae675559393739452bc171f77e798be838
msterhuj <gabin.lanore@gmail.com>
2021-07-24 17:01:35 +0200

    Added auto load for token from env

--- server/api/apps.py
+++ server/api/apps.py
@@ -1,5 +1,17 @@
+import django.db.utils
 from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+
+def register_token_from_env() -> None:
+    from server.api.models.Token import Token
+    from os import environ
+    token_list = environ.get("TOKEN").split(",")
+    for tk in token_list:
+        try:
+            Token(token=tk).save()
+        except django.db.utils.IntegrityError:
+            pass
