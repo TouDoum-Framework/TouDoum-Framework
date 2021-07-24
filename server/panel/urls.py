f0e5e94b6ca77bbc9af8fef78771303ad4057732
msterhuj <gabin.lanore@gmail.com>
2021-07-24 12:47:40 +0200

    Added func to add ip on db

--- server/panel/urls.py
+++ server/panel/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from server.modules.apps import get_urls
-from server.panel.views import auth
+from server.panel.views import auth, modules, ipdb
 from server.panel.views.index import index
-from server.panel.views import modules
 
 
 urlpatterns = [
     path('', auth.login_view, name="login"),
     path('logout', auth.logout_view, name="logout"),
     path('panel', index, name="index"),
-    path('panel/modules', modules.index, name="modules_index")
+    path('panel/modules', modules.index, name="modules_index"),
+    path('panel/ipdb', ipdb.index, name="ipdb_index")
 ] + get_urls("panel")
