82dc4be5e572fe3386133f41b241ededf40b78ed
msterhuj <gabin.lanore@gmail.com>
2021-12-21 23:02:34 +0100

    Feat: Optimization of the client loader

--- client/TouDoumClient.py
+++ client/TouDoumClient.py
@@ -1,10 +1,35 @@
-import os
+from socket import gethostname
+from os import environ
+from celery import Celery
 from dotenv import load_dotenv
-from client.core.Client import Client
 
-if os.environ.get("MODE") is None:
+from client.core.Api import Api
+from client.core.ModuesLoader import ModulesLoader
+
+
+class Client:
+    hostname: str
+    api: Api
+    moduleLoader: ModulesLoader
+    celery: Celery
+
+    def __init__(self, celery):
+        print("Initialization of client")
+        self.hostname = gethostname()
+        self.api = Api()
+        self.celery = celery
+
+        print("Client init success starting Celery worker")
+
+
+if environ.get("MODE") is None:
     load_dotenv("../.env")
     print("Load env from .env file")
 
-client = Client()
-from client.core.client_exec_task import *
+celery = Celery('tasks', broker='pyamqp://{}:{}@{}//'.format(
+    environ.get("MQ_USER", "white"),
+    environ.get("MQ_PASS", "neo"),
+    environ.get("MQ_HOST", "127.0.0.1")
+))
+celery.autodiscover_tasks(['client.core'])
+#client = Client(celery)
