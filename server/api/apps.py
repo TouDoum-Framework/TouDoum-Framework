d3cc21df5fd72b2e6959a8cff8fa2070bb37debc
msterhuj <gabin.lanore@gmail.com>
2021-12-21 20:06:41 +0100

    Fix: postgresql error unique key on django load

--- server/api/apps.py
+++ server/api/apps.py
@@ -1,4 +1,3 @@
-import django.db.utils
 from django.apps import AppConfig
 
 
@@ -10,8 +9,7 @@ def register_token_from_env() -> None:
     from server.api.models.Token import Token
     from os import environ
     token_list = environ.get("TOKEN").split(",")
-    for tk in token_list:
-        try:
+    for tk_str in token_list:
+        tk = Token.objects.filter(token=tk_str).first()
+        if tk is None:
             Token(token=tk).save()
-        except django.db.utils.IntegrityError:
-            pass
