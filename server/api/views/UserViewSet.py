4453e6ce5d9ab1f3f043a3c68f47da3da35f4928
msterhuj <gabin.lanore@gmail.com>
2022-07-31 22:45:00 +0200

    feat: set user password from api

--- server/api/views/UserViewSet.py
+++ server/api/views/UserViewSet.py
@@ -7,13 +7,23 @@ from server.api.views.PermissionViewSet import PermissionSerializer
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-
     permissions = PermissionSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ["url", "username", "first_name", "last_name", "email",
-                  "groups", "permissions", "is_staff", "is_active", "date_joined"]
+        fields = ["url", "username", "first_name", "last_name", "email", "password", "groups", "permissions",
+                  "is_staff", "is_active", "date_joined"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def save(self, **kwargs):
+        user = super().save(**kwargs)
+        password = self.validated_data.get('password', None)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,5 +32,5 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        queryset = User.objects.all()
+        queryset = User.objects.all().order_by("pk")
         return queryset
